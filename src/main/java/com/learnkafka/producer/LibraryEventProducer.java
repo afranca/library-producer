29ea40c17b10e2f6216cfe52b7dc3c53628d459b
Alexandre Franca <Alexandre.Franca@bjss.com>
2022-01-22 14:48:04 +0000

    Adding RecordHeader to ProducerRecord. (Message Metadate)

--- src/main/java/com/learnkafka/producer/LibraryEventProducer.java
+++ src/main/java/com/learnkafka/producer/LibraryEventProducer.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.learnkafka.domain.LibraryEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.Header;
+import org.apache.kafka.common.header.internals.RecordHeader;
 import org.apache.kafka.common.protocol.types.Field;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -13,6 +15,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -69,7 +72,7 @@ public class LibraryEventProducer {
     }
 
     private ProducerRecord<Integer,String> buildProducerRecord(Integer key, String value, String topic) {
-
+        List<Header> recordHeaders = List.of(new RecordHeader("event-source", "scanner".getBytes()));
         return new ProducerRecord<>(topic,null, key, value, null);
     }
 
