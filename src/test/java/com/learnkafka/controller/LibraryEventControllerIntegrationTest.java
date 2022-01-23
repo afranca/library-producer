00b9af9675977ec91899cc6c9def8dfcec31672c
Alexandre Franca <Alexandre.Franca@bjss.com>
2022-01-23 08:24:53 +0000

    Removing @EmbeddedKafka @TestPropertySource as Embedded Kafka is not working, 'kafka/common/KafkaException NoClassDefFoundError'

--- src/test/java/com/learnkafka/controller/LibraryEventControllerIntegrationTest.java
+++ src/test/java/com/learnkafka/controller/LibraryEventControllerIntegrationTest.java
@@ -8,10 +8,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.Date;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@EmbeddedKafka(topics = {"library-events"}, partitions = 3)
+//@TestPropertySource(properties = {"spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}",
+//        "spring.kafka.admin.properties.bootstrap.servers=${spring.embedded.kafka.brokers}"})
 public class LibraryEventControllerIntegrationTest {
 
     @Autowired
@@ -22,7 +31,7 @@ public class LibraryEventControllerIntegrationTest {
         //given
         Book book = Book.builder()
                 .bookId(123)
-                .bookAuthor("Dilip")
+                .bookAuthor("Dilip"+ new Random().nextInt(1000))
                 .bookName("Kafka In Action")
                 .build();
 
